0e3e693f2db2879f58d155c47cec49bcb49e3484
114264 - Moszoro Mateo <114264@tecnicatura.frc.utn.edu.ar>
2024-10-05 16:50:58 -0300

    AlgunosTestCompletados

--- src/test/java/ar/edu/utn/frc/tup/lciii/service/CountryServiceTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lciii/service/CountryServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.internal.matchers.CapturesArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -35,25 +36,50 @@ class CountryServiceTest {
     private List<Country> countries;
     @BeforeEach
     void init(){
+        Map<String,String> lenguaje = Map.of("RUS","Russian");
+
+
         countries = new ArrayList<>();
         Country country1 = new Country();
         country1.setName("Rusia");
         country1.setCode("RUS");
+        country1.setRegion("Europa");
+        country1.setLanguages(lenguaje);
         Country country2 = new Country();
         country2.setName("Argentina");
         country2.setCode("ARG");
+        country2.setRegion("Americas");
         Country country3 = new Country();
         country3.setName("Brazil");
         country3.setCode("BRA");
+        country3.setRegion("Americas");
 
         countries.add(country1);
         countries.add(country2);
         countries.add(country3);
     }
 
+    @DisplayName("Traer countries formateadas a dto")
     @Test
-    @Disabled
     void getAllCountries() {
+        List<CountryDTO> respuesta = new ArrayList<>();
+        String url = "https://restcountries.com/v3.1/all";
+        List<Map<String, Object>> response = new ArrayList<>();
+
+        when(restTemplate.getForObject(url, List.class)).thenReturn(response);
+        when(countryService.getAllCountries()).thenReturn(countries);
+        List<Country> paises = countryService.getAllCountries();
+        for (Country c : paises){
+            respuesta.add(ReflectionTestUtils.invokeMethod(countryService,"mapToDTO",c));
+        }
+        assertEquals(respuesta.size(),countries.size());
+        assertEquals(respuesta.get(0).getCode(),countries.get(0).getCode());
+        assertEquals(respuesta.get(0).getName(),countries.get(0).getName());
+        assertEquals(respuesta.get(1).getCode(),countries.get(1).getCode());
+        assertEquals(respuesta.get(1).getName(),countries.get(1).getName());
+        assertEquals(respuesta.get(2).getCode(),countries.get(2).getCode());
+        assertEquals(respuesta.get(2).getName(),countries.get(2).getName());
+
     }
 
     @Test
@@ -77,14 +103,43 @@ class CountryServiceTest {
 
     @Test
     void getCountriesByContinent() {
+        //given
+        String continent = "Americas";
+        String url = "https://restcountries.com/v3.1/all";
+        List<Map<String, Object>> response = new ArrayList<>();
+        when(restTemplate.getForObject(url, List.class)).thenReturn(response);
+        when(countryService.getAllCountries()).thenReturn(countries);
+
+        //when
+        List<CountryDTO> respuesta = countryService.getCountriesByContinent(continent);
+
+        //then
+        assertEquals(respuesta.size(),2);
+        assertEquals(respuesta.get(0).getName(),countries.get(1).getName());
+        assertEquals(respuesta.get(1).getName(),countries.get(2).getName());
     }
 
     @Test
     void getCountryByLanguage() {
+        //given
+        String languaje = "Russian";
+        String url = "https://restcountries.com/v3.1/all";
+        List<Map<String, Object>> response = new ArrayList<>();
+        when(restTemplate.getForObject(url, List.class)).thenReturn(response);
+        when(countryService.getAllCountries()).thenReturn(countries);
+
+        //when
+        List<CountryDTO> respuesta = countryService.getCountryByLanguage(languaje);
+
+        //then
+        assertEquals(respuesta.size(),1);
+        assertEquals(respuesta.get(0).getName(),countries.get(0).getName());
+        assertEquals(respuesta.get(0).getCode(),countries.get(0).getCode());
     }
 
     @Test
     void getCountryMostBorder() {
+
     }
 
     @Test
